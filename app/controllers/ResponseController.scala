4c1baafaec83f9c6d5b9eb433a12116c20803db4
tomoyaueno29 <u.t012807@gmail.com>
2020-08-24 22:22:58 +0900

    Create ResponseController.scala

---
+++ app/controllers/ResponseController.scala
@@ -0,0 +1,15 @@
+package controllers
+
+import play.api.libs.functional.syntax.unlift
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
+
+case class Meta(status: Int, errorMessage: String)
+
+object Meta {
+  implicit val metaWrites = (
+    (__ \ "status").write[Int] and
+      (__ \ "errorMessage").write[String]
+    )(unlift(Meta.unapply))
+}
+
